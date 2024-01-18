9a33b6bbd717b52768d7a6872a0ea618879d5a44
CarlosArrieta <carrieta@defontana.com>
2024-01-18 18:57:12 -0300

    add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,13 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-tutorial-carlitonchin.netlify.app/",
-  integrations: [react()]
+  integrations: [react()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es", "pt-br"],
+    fallback:{
+      es:"en",
+      "pt-br":"en"
+    }
+  }
 });
